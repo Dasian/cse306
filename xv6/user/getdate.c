6b7f8b2b4fedd21c886ad4cb13e75458b600f3bf
Dasian <davidrespitu99@gmail.com>
2021-02-28 13:40:57 -0500

    Moved getdate and setdate and updated headers

--- xv6/user/getdate.c
+++ xv6/user/getdate.c
@@ -11,28 +11,15 @@
   TODO 
   	Test and debug this function
   	Add the proper includes
+    Change year to 21 century?
   	Figure out how to connect to console
+    Write in proc.c??
 */
-int getdate(struct rtcdate *r) {
-  
-  // checks if pointer is valid
-  if(sizeof(*r) != sizeof(struct rtcdate))
-    return -1;
-  if(argptr(1, (void*) &r, sizeof(struct rtcdate)) != 0)
-    return -1;
-  if(sys_fstat() == -1)
-    return -1;
-
-  // retrieves date and time and writes it into r
-  cmostime(r);
-  return 0;
-
-}
 
 int main(int argc, char* argv[]) {
 	struct rtcdate tmp;
 	if(getdate(&tmp)) 
 		printf("%s\n", "getdate failed");
-	printf("Year %i Month %i Day %i %i:%i:%i",tmp.year, tmp.month, tmp.day, tmp.hour, tmp.min, tmp.sec);
+	printf("Date: %i/%i/%i Time: %i %i:%i:%i",tmp.month, tmp.day, tmp.year, tmp.hour, tmp.min, tmp.sec);
 	exit();
 }
--- xv6/user/setdate.c
+++ xv6/user/setdate.c
@@ -4,21 +4,6 @@
 #include "user.h"
 #include "kernel/lapic.c"
 
-/* 
-  Added for hw1
-  Returns x in bcd format
-  ex 12 -> 0001 0010
-*/
-uint tobcd(uint x) {
-  uint conv = 0, i=0;
-  while(x > 0){
-    conv += (x%10 << 4*i);
-    x /= 10;
-    i++;
-  }
-  return conv;
-}
-
 /*
   Added for hw1
   Obtains user info to set the date and time of the system
@@ -26,62 +11,10 @@ uint tobcd(uint x) {
   TODO
     Check validation ranges
     Check leap years
-    Figure out the proper register numbers to write into
     Test and debug
     Figure out how to connect to console
+    MOVED TO proc.c
 */
-int setdate(struct rtcdate *r) {
-  // validate pointer
-  if(sizeof(*r) != sizeof(struct rtcdate))
-    return -1;
-  if(argptr(1, (void*) &r, sizeof(struct rtcdate)) != 0)
-    return -1;
-  if(sys_fstat() == -1)
-    return -1;
-
-  // validate pointer fields
-  uint second, minute, hour, month, day, year;
-  int isleap = 0;
-  uint max_day[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-
-  if((second=r->second) < 0 || r->second >= 60)   return -1;
-  if((minute=r->minute) < 0 || r->minute >= 60)   return -1;
-  if((hour=r->hour) < 0 || r->hour >= 23)         return -1;
-  if((month=r->month) <=0 || month > 12)          return -1;
-  if((day=r->day) <= 0 || day > 31)               return -1;
-  if(!isleap && (day > max_day[month-1]))         return -1;
-  if(isleap && month == 2 && day > 29)	s		  return -1; // leap year check
-
-  // encode base 10 values into binary code decimal (bcd)
-  second = tobcd(second);
-  minute = tobcd(minute);
-  hour = tobcd(hour);
-  month = tobcd(month);
-  day = tobcd(day);
-  year = tobcd(r->year);
-
-  // prevent interrupts
-  pushcli();
-
-  // write values to the registers
-  outb(0x70, 1<<7 | YEAR);
-  outb(0x71, year);
-  outb(0x70, 1<<7 | MONTH);
-  outb(0x71, MONTH);
-  outb(0x70, 1<<7 | DAY);
-  outb(0x71, day);
-  outb(0x70, 1<<7 | HOURS);
-  outb(0x71, hour);
-  outb(0x70, 1<<7 | MINS);
-  outb(0x71, minute);
-  outb(0x70, 1<<7 | SECS);
-  outb(0x71, second);
-
-
-  popcli();
-
-  return 0;
-}
 
 int main(int argc, char* argv[]) {
 	// run user input checks
