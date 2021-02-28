f046087bcdf2ac05576882b30fee568419ffe0dc
Dasian <davidrespitu99@gmail.com>
2021-02-28 14:38:58 -0500

    Added basic timerrate driver skeleton

--- xv6/user/timerrate.c
+++ xv6/user/timerrate.c
@@ -1,21 +1,21 @@
 #include "kernel/types.h"
 #include "kernel/stat.h"
+#include "defs.h"
 #include "user.h"
 
 /*
 	Added for hw1
 	TODO
-		Figure out how to count ticks
-		Figure out how to change the target and countdown var
-		Figure out how to connect that to this func
+		Figure out how to test and link timerrate func
+		func is found in proc.c and sys_timerrate is found in syscall.c
 */
-int timerrate(int *hz) {
-	return -1;
-}
 
 int main(int argc, char* argv[]) {
-	if(argc < 2)
-		printf("%s\n", "Usage: timerrate [hz]");
-	timerrate(argv[1]);
+	if(argc < 2) {
+		printf(1, "%s\n", "Usage: timerrate [hz]");
+		exit();
+	}
+	int prev=timerrate(argv[1])
+	printf(1, "Old target: %d New target: %d Return Value: %d",prev, *argv[1], prev);
 	exit();
 }
