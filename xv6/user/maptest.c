cc77403b749f4e5e0a27ee7bb9a3dea7d32b2f4c
Dasian <davidrespitu99@gmail.com>
2021-05-08 19:11:06 -0400

    Added instructions for what to implement

--- xv6/user/maptest.c
+++ xv6/user/maptest.c
@@ -26,6 +26,35 @@ int main(int argc, char* argv[]) {
 	#endif
 
 	#if HW5_userprog
-	// 
+
+	// All of these require multi process execution
+	// By completing these it is implied that the same
+	// operations work on a single proc execution
+
+	// Show COW execution:
+	// Show contents of table of proc 1 (parent)
+	// Fork and show contents of table for proc 2 (child)
+	// These should be the same
+	// Have proc 2 (child) write
+	// Show the contents of table for both procs
+	// They should be different
+	
+	// Show private files:
+	// Both load the same file
+	// proc 1 writes to the file
+	// Both print file
+	// Should be different
+
+	// Show shared memory:
+	// Allocate shared anon mem
+	// Have proc 1 write
+	// Have proc 2 read
+	// Should show data written by proc 1
+
+	// Show private memory:
+	// Both allocate private memory
+	// Both write to it
+	// Both read it
+	// Should be different
 	#endif
 }
